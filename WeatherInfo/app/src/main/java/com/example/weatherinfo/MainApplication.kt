05a8b6f2248e9dbbaade61692bef5730cb28a756
Shobhit Sharma <sharmashobhit@edu.bme.hu>
2025-01-16 20:08:43 +0100

    Delete WeatherInfo directory

--- WeatherInfo/app/src/main/java/com/example/weatherinfo/MainApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.example.weatherinfo
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MainApplication : Application(){
-}
--- WeatherInfo/app/src/main/java/com/example/weatherinfo/ui/navigation/MainNavigation.kt
+++
@@ -1,6 +0,0 @@
-package com.example.weatherinfo.ui.navigation
-
-sealed class Screen(val route: String) {
-    object Main : Screen("main")
-    object Weather : Screen("weather")
-}
